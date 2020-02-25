4d5f1495e5e68e92a2816e05b02e322c0a10b25d
Mehrad Kavian <mehrad.kavian@gmail.com>
2020-02-25 17:42:08 -0500

    didregister is done and tested

--- examples/nodejs/testTxBuilder.js
+++ examples/nodejs/testTxBuilder.js
@@ -11,24 +11,36 @@ async function createWallet() {
   let wallet = await Metaverse.wallet.fromMnemonic(mnemonic, "testnet");
 
   addresses = await wallet.getAddresses(); // get list of addresses
-  return wallet
+  return wallet;
   // console.log(addresses)
 }
 
-transferETP = async () => {
-  let tx = await txBuilder.ETPTransferTx(
-    "tTe1ZzgxtZ8Q6o972DJbvHTPg3Bja5wydc",
-    100000000,
+// transferETP = async () => {
+//   let tx = await txBuilder.ETPTransferTx(
+//     "tTe1ZzgxtZ8Q6o972DJbvHTPg3Bja5wydc",
+//     100000000,
+//     "tD2tYRPZkUjTWx4RtGMKj5M1j8cJdzBR47"
+//   );
+//   let wallet = await createWallet();
+//   tx = await wallet.sign(tx);
+//   tx = await tx.encode();
+//   tx = await blockchain.transaction.broadcast(tx.toString("hex"));
+//   console.log("txHash in test file ==>\n")
+// };
+
+// transferETP();
+
+registerAvatar = async () => {
+  let tx = await txBuilder.didRegisterTx(
+    "lilSister",
     "tD2tYRPZkUjTWx4RtGMKj5M1j8cJdzBR47"
   );
+  console.log("didRegister tx in test file => \n", tx);
   let wallet = await createWallet();
   tx = await wallet.sign(tx);
-
-  //Encode the transaction into bytecode
   tx = await tx.encode();
-
-  //Broadcast the transaction to the metaverse network.
   tx = await blockchain.transaction.broadcast(tx.toString("hex"));
+  console.log("txHash in test file ==>\n", tx);
 };
 
-transferETP();
+registerAvatar();
--- examples/nodejs/txBuilder.js
+++ examples/nodejs/txBuilder.js
@@ -12,13 +12,10 @@ const ETPTransferTx = async (sender, amount, recipient) => {
 
   let txs = await blockchain.addresses.txs([sender]);
 
-  //Get all utxo
   let utxos = await Metaverse.output.calculateUtxo(txs.transactions, [sender]);
 
-  //Collect enough utxos to pay for the transfer
   let result = await Metaverse.output.findUtxo(utxos, target, height);
 
-  //Build the transaction object
   let tx = await Metaverse.transaction_builder.send(
     result.utxo,
     recipient,
@@ -30,4 +27,25 @@ const ETPTransferTx = async (sender, amount, recipient) => {
   return tx;
 };
 
+const didRegisterTx = async (avatar_symbol, avatar_address) => {
+  let change_address = avatar_address;
+  let height = await blockchain.height();
+  let txs = await blockchain.addresses.txs([avatar_address]);
+  let utxos = await Metaverse.output.calculateUtxo(txs.transactions, [
+    avatar_address
+  ]); //Get all utxo for the avatar address
+  let result = await Metaverse.output.findUtxo(utxos, {}, height, 100000000); //Collect utxo to pay for the fee of 1 ETP
+  let tx = await Metaverse.transaction_builder.issueDid(
+    result.utxo,
+    avatar_address,
+    avatar_symbol,
+    change_address,
+    result.change,
+    80000000,
+    "testnet"
+  );
+  return tx;
+};
+
 module.exports.ETPTransferTx = ETPTransferTx;
+module.exports.didRegisterTx = didRegisterTx;
